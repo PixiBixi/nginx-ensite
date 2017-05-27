100703c921debb92da95c6260ee4f5c787950d50
PixiBixi <PixiBixi@users.noreply.github.com>
2017-05-27 12:00:26 +0200

    [FIX] Color reset

--- nginx_ensite.sh
+++ nginx_ensite.sh
@@ -19,24 +19,25 @@ BLUE="\033[34m"
 RED="\033[31m"
 YELLOW="\033[33m"
 GREEN="\033[32m"
+CEND="\033[0m"
 # END OF COLORS #
 
 if [[ $UID != 0 ]]; then
-	echo -e "${RED}${WHITE_BOLD}$NAME doit être exécuté en root"
+	echo -e "${RED}${WHITE_BOLD}$NAME doit être exécuté en root${CEND}"
 	exit 1;
 fi
 
 function preCheck(){
 	if [[ ! -d $NGINX_AVAILABLE ]]; then
-		echo -e "${RED}${WHITE_BOLD}$NGINX_AVAILABLE inexistant"
+		echo -e "${RED}${WHITE_BOLD}$NGINX_AVAILABLE inexistant${CEND}"
 	fi
 
 	if [[ ! -d $NGINX_ENABLED ]]; then
-		echo -e "${RED}${WHITE_BOLD}$NGINX_ENABLED inexistant"
+		echo -e "${RED}${WHITE_BOLD}$NGINX_ENABLED inexistant${CEND}"
 	fi
 
 	if ! [[ $(dpkg -s nginx | grep Status ) =~ "Status: install ok installed" ]]  &> /dev/null ; then
-		echo -e "${RED}${WHITE_BOLD}nGinx n'est pas installé"
+		echo -e "${RED}${WHITE_BOLD}nGinx n'est pas installé${CEND}"
 	fi
 }
 
@@ -45,29 +46,29 @@ case $1 in
 		preCheck
 			# Si Argument
 			if [[ -z $2 ]]; then
-				echo -e "${RED}${WHITE_BOLD}Utilisation : $NAME enable nginx_block"
+				echo -e "${RED}${WHITE_BOLD}Utilisation : $NAME enable nginx_block${CEND}"
 				exit 1
 			fi
 			# Si fichier inexistant $SITES_AVAILABLE
 			if [[ ! -f "$NGINX_AVAILABLE/$2" ]]; then
-				echo -e "${RED}${WHITE_BOLD}Fichier $2 inexistant dans $NGINX_AVAILABLE"
+				echo -e "${RED}${WHITE_BOLD}Fichier $2 inexistant dans ${NGINX_AVAILABLE}${CEND}"
 				exit 1
 			# Si fichier existant $SITES_ENABLED
 			elif [[ -f "$NGINX_ENABLED/$2" ]]; then
-				echo -e "${RED}${WHITE_BOLD}Fichier $2 déjà activé"
+                echo -e "${RED}${WHITE_BOLD}Fichier $2 déjà activé${CEND}"
 				exit 1
 			# Sinon
 			else
-				echo -e "${GREEN}${WHITE_BOLD}Fichier $2 existant"
+				echo -e "${GREEN}${WHITE_BOLD}Fichier $2 existant${CEND}"
 				# Si fichier existant $SITES_ENABLED
 				if [[ -f "$NGINX_ENABLED/$2" ]]; then
-					echo -e "${RED}${WHITE_BOLD}Fichier $2 déjà activé"
+					echo -e "${RED}${WHITE_BOLD}Fichier $2 déjà activé${CEND}"
 					exit 1
 				else
 					ln -s "$NGINX_AVAILABLE/$2" "$NGINX_ENABLED/$2"
-					echo -e "${GREEN}${WHITE_BOLD}Block $2 activé"
+					echo -e "${GREEN}${WHITE_BOLD}Block $2 activé${CEND}"
 					$SERVICE nginx restart &> /dev/null
-					echo -e "${GREEN}${WHITE_BOLD}nGinx redémarré"
+					echo -e "${GREEN}${WHITE_BOLD}nGinx redémarré${CEND}"
 				fi
 			fi
 		;;
@@ -75,33 +76,33 @@ case $1 in
 	disable )
 		preCheck
 			if [[ -z $2 ]]; then
-				echo -e "${RED}${WHITE_BOLD}Utilisation : $NAME enable nginx_block"
+				echo -e "${RED}${WHITE_BOLD}Utilisation : $NAME enable nginx_block${CEND}"
 				exit 1
 			fi
 
 			if [[ ! -f "$NGINX_ENABLED/$2" ]]; then
-				echo -e "${RED}${WHITE_BOLD}Fichier $2 inexistant dans $NGINX_ENABLED"
+				echo -e "${RED}${WHITE_BOLD}Fichier $2 inexistant dans ${NGINX_ENABLED}${CEND}"
 				exit 1
 			# Sinon (Si un fichier existe)
 			else
 				if [[ -L "$NGINX_ENABLED/$2" ]]; then # Lien symbolique + fichier existe
-					echo -e "${GREEN}Fichier $NGINX_ENABLED/$2 supprimé"
+					echo -e "${GREEN}Fichier $NGINX_ENABLED/$2 supprimé${CEND}"
 					/bin/rm "$NGINX_ENABLED/$2"
 					$SERVICE nginx restart
-					echo -e "${GREEN}Redémarrage de nginx"
+					echo -e "${GREEN}Redémarrage de nginx${CEND}"
 				elif [[ -h "$NGINX_ENABLED/$2" ]]; then # Lien symbolique + fichier inexexistant
-					echo -e "${RED}Attention: Fichier ${SITES_AVAILABLE}/${2} inexexistant"
+					echo -e "${RED}Attention: Fichier ${SITES_AVAILABLE}/${2} inexexistant${CEND}"
 					/bin/rm "$NGINX_ENABLED/$2"
 				else
-					echo -e -n "${RED}$NGINX_ENABLED/$2 n'est pas un lien symbolique vers $SITES_AVAILABLE, souhaitez-vous le supprimer ? (YES|NO)"
+					echo -e -n "${RED}$NGINX_ENABLED/$2 n'est pas un lien symbolique vers $SITES_AVAILABLE, souhaitez-vous le supprimer ? (YES|NO)${CEND}"
 					read CHOICE_RM
 					if [[ $CHOICE_RM =~ yes ]]; then
 						/bin/rm "$NGINX_ENABLED/$2"
-						echo -e "${GREEN}$NGINX_ENABLED/$2 a été supprimé"
+						echo -e "${GREEN}$NGINX_ENABLED/$2 a été supprimé${CEND}"
 						$SERVICE nginx restart
-						echo -e "${GREEN}Redémarrage de nginx"
+						echo -e "${GREEN}Redémarrage de nginx${CEND}"
 					else
-						echo -e "${RED}$NGINX_ENABLED/$2 n'a pas été supprimé"
+						echo -e "${RED}$NGINX_ENABLED/$2 n'a pas été supprimé${CEND}"
 						exit 1
 					fi
 				fi
@@ -121,7 +122,7 @@ case $1 in
 
 	* )
 		echo -e "${YELLOW}${WHITE_BOLD}Usages possibles :"
-		echo -e "${WHITE_BOLD}${BLUE}enable :${WHITE} enable nginx block"
-		echo -e "${WHITE_BOLD}${BLUE}disable :${WHITE} disable nginx block"
-		echo -e "${WHITE_BOLD}${BLUE}list :${WHITE} list all nginx blocks"
+		echo -e "${WHITE_BOLD}${BLUE}enable :${WHITE} enable nginx block${CEND}"
+		echo -e "${WHITE_BOLD}${BLUE}disable :${WHITE} disable nginx block${CEND}"
+		echo -e "${WHITE_BOLD}${BLUE}list :${WHITE} list all nginx blocks${CEND}"
 esac
